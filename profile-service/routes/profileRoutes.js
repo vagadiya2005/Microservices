33b1fb1f9c8ee75a10cb61cf258281fd7def1abf
Prince Vagadiya <147682826+vagadiya2005@users.noreply.github.com>
2024-08-29 08:54:15 +0530

    Update profileRoutes.js

--- profile-service/routes/profileRoutes.js
+++ profile-service/routes/profileRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {add , all ,search} = require('../controller/profileController');
 
 router.post('/api/v1/profile/add',add);
-router.get('/api/v1/profile/all',allll);
+router.get('/api/v1/profile/allll',all);
 router.get('/api/v1/profile/search',search);
 
 
