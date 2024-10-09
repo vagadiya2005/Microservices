b97df541831eb4364c9484960a391749e8b685d8
vagadiya2005 <princevagadiya2005@gmail.com>
2024-10-09 15:12:09 +0530

    add prom client

--- profile-service/index.js
+++ profile-service/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const client = require('prom-client'); // Prometheus client
 require('dotenv').config();
 app.use(express.json());
 
@@ -8,6 +9,30 @@ app.use(express.json());
 
 require('./database');
 const port = process.env.PORT || 3000;
+
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Create a counter for the number of requests
+const requestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'endpoint'],
+});
+register.registerMetric(requestCounter);
+
+// Middleware to count the requests
+app.use((req, res, next) => {
+  requestCounter.inc({ method: req.method, endpoint: req.path });
+  next();
+});
+
+// Endpoint to expose metrics
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 // middleware
    
