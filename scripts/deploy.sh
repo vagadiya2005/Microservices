022907784f36b853542b95eb32d1450928710b86
vagadiya2005 <princevagadiya2005@gmail.com>
2024-10-29 11:32:54 +0530

    optimise deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,6 @@
 # Load environment variables from .env file
 source .env
 
-# Docker Hub Credentials (Environment variables)
-# REPO_URL="https://github.com/vagadiya2005/Microservices.git"
-# PROJECT_DIR="Microservices"
-
 # Pull code from the remote repository
 echo "Pulling code from remote repository..."
 if [ -d "$PROJECT_DIR" ]; then
@@ -21,25 +17,30 @@ fi
 echo "Logging into Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Build, tag, and push Docker images for each service
+# Build, tag, and push Docker images for each service in parallel
 declare -A services=( ["auth-service"]="auth-service-img" ["profile-service"]="profile-service-img" ["nginx"]="nginx-service-img" )
 
 for service in "${!services[@]}"; do
-  echo "Building Docker image for $service..."
-  
-  if [ -d "$service" ]; then
-    cd $service || exit
-    docker build -t "${services[$service]}" .
+  (
+    echo "Building Docker image for $service..."
+    
+    if [ -d "$service" ]; then
+      cd $service || exit
+      docker build -t "${services[$service]}" .
 
-    echo "Tagging and pushing Docker image for $service to Docker Hub..."
-    docker tag "${services[$service]}:latest" "$DOCKER_USERNAME/${services[$service]}:latest"
-    docker push "$DOCKER_USERNAME/${services[$service]}:latest"
-    cd ..
-  else
-    echo "Error: Directory $service does not exist!"
-    exit 1
-  fi
+      echo "Tagging and pushing Docker image for $service to Docker Hub..."
+      docker tag "${services[$service]}:latest" "$DOCKER_USERNAME/${services[$service]}:latest"
+      docker push "$DOCKER_USERNAME/${services[$service]}:latest"
+      cd ..
+    else
+      echo "Error: Directory $service does not exist!"
+      exit 1
+    fi
+  ) &
 done
+
+# Wait for all background processes to complete
+wait
 
 # Deploy using docker-compose
 echo "Deploying with docker-compose..."
