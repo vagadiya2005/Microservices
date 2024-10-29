a434bd81cf17010a1c7b12e429a7e85e27c03bb9
vagadiya2005 <princevagadiya2005@gmail.com>
2024-10-29 11:12:24 +0530

    modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# Load environment variables from .env file
 source .env
-# Docker Hub Credentials (Set these as environment variables for security)
-# DOCKER_USERNAME="${DOCKER_USERNAME:-your_dockerhub_username}"
-# DOCKER_PASSWORD="${DOCKER_PASSWORD:-your_dockerhub_password}"
+
+# Docker Hub Credentials (Environment variables)
 # REPO_URL="https://github.com/vagadiya2005/Microservices.git"
 # PROJECT_DIR="Microservices"
-
 
 # Pull code from the remote repository
 echo "Pulling code from remote repository..."
@@ -18,22 +17,28 @@ else
   cd $PROJECT_DIR
 fi
 
-# Docker login
+# Docker login with --password-stdin to avoid non-TTY error
 echo "Logging into Docker Hub..."
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build, tag, and push Docker images for each service
 declare -A services=( ["auth-service"]="auth-service-img" ["profile-service"]="profile-service-img" ["nginx"]="nginx-service-img" )
 
 for service in "${!services[@]}"; do
   echo "Building Docker image for $service..."
-  cd $service || exit
-  docker build -t "${services[$service]}" .
+  
+  if [ -d "$service" ]; then
+    cd $service || exit
+    docker build -t "${services[$service]}" .
 
-  echo "Tagging and pushing Docker image for $service to Docker Hub..."
-  docker tag "${services[$service]}:latest" "$DOCKER_USERNAME/${services[$service]}:latest"
-  docker push "$DOCKER_USERNAME/${services[$service]}:latest"
-  cd ..
+    echo "Tagging and pushing Docker image for $service to Docker Hub..."
+    docker tag "${services[$service]}:latest" "$DOCKER_USERNAME/${services[$service]}:latest"
+    docker push "$DOCKER_USERNAME/${services[$service]}:latest"
+    cd ..
+  else
+    echo "Error: Directory $service does not exist!"
+    exit 1
+  fi
 done
 
 # Deploy using docker-compose
